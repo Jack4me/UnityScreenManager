6a70e3538b9d2dc88dd7fc22dad9bcb5d6e6d16c
Jack4me <just4go@gmail.com>
2024-09-07 14:59:02 +0200

    +3

--- Assets/CodeBase/ScreenManager/CloseButton.cs
+++ Assets/CodeBase/ScreenManager/CloseButton.cs
@@ -1,9 +1,15 @@
+using System;
 using UnityEngine;
-
+using UnityEngine.UI;
 namespace CodeBase.ScreenManager {
    public class CloseButton : MonoBehaviour {
-      public GameObject window;
+      public GameObject window; 
+      public Button CloseHUDButton;
 
+
+      private void Start() {
+         CloseHUDButton.onClick.AddListener(ScreenManager.instance.GoBack);
+      }
 
       public void CloseWindow() {
          window.SetActive(false);
---
+++ Assets/CodeBase/ScreenManager/ScreenManager.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeBase.ScreenManager {
+    public class ScreenManager : MonoBehaviour {
+        [SerializeField] private GameObject HUD;
+        public static ScreenManager instance;
+        public Stack<GameObject> screenHistory = new Stack<GameObject>();
+
+        void Awake() {
+            mainMenuScreen.SetActive(true);
+            screenHistory.Push(mainMenuScreen);
+
+            if (instance != null) {
+                Destroy(gameObject);
+            }
+            else {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+
+
+            HUD = Instantiate(HUD);
+            InitializeHUD(HUD);
+        }
+
+        public GameObject mainMenuScreen;
+        public GameObject settingsScreen;
+        public GameObject pauseMenuScreen;
+        public GameObject shopScreen;
+
+        public void InitializeHUD(GameObject HUD) {
+            settingsScreen = HUD.transform.Find("SettingsScreen").gameObject;
+
+
+            Debug.Log("HUD инициализирован.");
+        }
+
+        public void ShowScreen(string panelName) {
+            HideAllPanels();
+
+            GameObject screen = null; // Для хранения текущего экрана
+
+            // Находим нужную панель по имени
+            switch (panelName) {
+                case "MainMenu":
+                    screen = mainMenuScreen;
+                    break;
+                case "Settings":
+                    screen = settingsScreen;
+                    break;
+                case "PauseMenu":
+                    screen = pauseMenuScreen;
+                    break;
+                case "Shop":
+                    screen = shopScreen;
+                    break;
+                default:
+                    Debug.LogError("Unknown panel: " + panelName);
+                    return;
+            }
+
+
+            if (screenHistory.Count > 0) {
+                screenHistory.Peek().SetActive(false);
+                Debug.LogError("screenHistory.Count 0");
+            }
+
+
+            screen.SetActive(true);
+            screenHistory.Push(screen);
+        }
+
+        private void HideAllPanels() {
+            mainMenuScreen.SetActive(false);
+            settingsScreen.SetActive(false);
+        }
+
+        public void GoBack() {
+            if (screenHistory.Count > 1) {
+                Debug.LogError("screenHistory.Count 01");
+                screenHistory.Pop().SetActive(false);
+                screenHistory.Peek().SetActive(true);
+            }
+        }
+    }
+}
